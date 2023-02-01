10c4e81d9eeb2e381a86d045e8f9802db1679e63
Housseem-Kouki <houssem.kouki@esprit.tn>
2023-02-01 01:06:27 +0100

    first comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SaluthiComponent } from './saluthi/saluthi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaluthiComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/saluthi/saluthi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saluthi',
+  templateUrl: './saluthi.component.html',
+  styleUrls: ['./saluthi.component.css']
+})
+export class SaluthiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
